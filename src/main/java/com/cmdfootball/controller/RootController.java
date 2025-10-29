f563db67175c78ace4994bf2cd1f1dee50e6ec14
Cmdcoachmoh <cmdfootsoccer@gmail.com>
2025-10-28 23:13:08 -0700

    add Root-level controller for Railway

---
+++ src/main/java/com/cmdfootball/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.cmdfootball.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CMD Football backend is alive!";
+    }
+}
